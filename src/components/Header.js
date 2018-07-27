8d7671306398b034fb7c568ffdafc5c3e07e5907
nicky evers <livelyevers@gmail.com>
2018-07-26 22:19:16 -0700

    Updated pages

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Nav from './Nav'
+
+const IndexPage = () => (
+  <div>
+    <Nav/>
+  </div>
+)
+
+export default IndexPage
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Nav = () => (
+  <div>
+    <Link to="/">Home</Link>
+  </div>
+)
+
+export default Nav
