f14f1a2f6b7a0315f2a32c503cccf0dd97b5cddf
nicky evers <livelyevers@gmail.com>
2018-08-09 10:37:20 -0700

    Partially add hero image with layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from '../../node_modules/styled-components';
+
+import heroImgSrc from '../../public/assets/img/stock/sprout-shadow.jpg';
+
+
+const HeroContainer = styled.div`
+  display: grid;
+  height: 1vh;
+  grid-template-columns: repeat(6, 1fr);
+  /* grid-template-rows: repeat; */
+  /* grid-gap: 2%; */
+`
+const HeroImg = styled.img`
+  display: grid;
+  grid-column: 1 / -1;
+  /* grid-row: 1; */
+`
 
 const IndexPage = () => (
-  <div>
-  </div>
+  <HeroContainer>
+    <HeroImg src={heroImgSrc} width="100%" height="auto"></HeroImg>
+  </HeroContainer>
 )
 
 export default IndexPage
