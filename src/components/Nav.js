5cc74684afbbd2cbf6fd695f34ed18b5bb95d6a5
nicky evers <livelyevers@gmail.com>
2018-10-02 18:19:39 -0700

    Add link hover transition and svg for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,59 +1,104 @@
 import React from 'react'
 import Link from 'gatsby-link'
-
 import styled from 'styled-components'
 
-const Container = styled.nav`
-  color: white;
+const NavWrapper = styled.div`
   display: grid;
-  grid-template-columns: 2fr 5fr .5fr;
-  grid-template-rows: 60px;
-  align-items: center;
-  text-shadow: 0 0 5px rgba(47, 47, 47, 0.25);
+  height: 100px;
+  grid-column: 3 / 8;
+  grid-row: 1 / 2;
+  grid-template-columns: 2fr 5fr 1fr;
 `
 
 const NavContainer = styled.ul`
-  height: 80px;
-  display: grid;
+  display: grid; 
+  grid-gap: 64px;
   grid-column: 2 / 3;
-  grid-row-gap: 1px;
-  grid-column-gap: 1px;
-  grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));
+  grid-template-columns: repeat(6, 1fr);
+  color: white;
+  text-shadow: 0 1px 5px rgba(47, 47, 47, 0.25);
+  padding-top: 20px;
 `
 
 const NavItem = styled.li`
   display: grid;
+  width: 110px;
+  text-align: center;
+  font-size: 18px;
+  letter-spacing: 3px;
+  align-self: center;
   justify-content: center;
-  font-size: .6em;
-  /* align-content: center; */
+  line-height: 23px;
   list-style-type: none;
-  /* font-weight: bold; */
-`
+  border-bottom: 5px solid transparent;
+  padding: 2px;
+  padding-bottom: 5px;
+  `
+
+  const NavSvg = styled.svg`
+    height: 10px;
+    /* visibility: hidden; */
+    opacity: 0;
+    transition: opacity 200ms ease;
+
+    ${NavItem}:hover & {
+      opacity: 1;
+    }
+  `;
+
+  const NavLine = styled.line`
+    stroke-linecap: round;
+    opacity: .9;
+  `;
 
 
 const Nav = () => (
-  <Container>
+  <NavWrapper>
     <NavContainer>
       <NavItem>
-        <Link to="/">HOME</Link>
+        <Link to="/">HOME
+          <NavSvg>
+            <NavLine x1="18" y1="5" x2="85" y2="5" stroke="white" strokeWidth="4" strokeLinecap="round"/>
+          </NavSvg>
+        </Link>
       </NavItem>
       <NavItem>
-        <Link to="/about">ABOUT</Link>
+        <Link to="/about">ABOUT
+          <NavSvg>
+            <NavLine x1="15" y1="5" x2="87" y2="5" stroke="white" strokeWidth="4" strokeLinecap="round"/>
+          </NavSvg>
+        </Link>
       </NavItem>
       <NavItem>
-        <Link to="/perinatal-loss">PERINATAL LOSS</Link>
+        <Link to="/perinatal-loss">PERINATAL LOSS
+          <NavSvg>
+            <NavLine x1="2" y1="5" x2="107" y2="5" stroke="white" strokeWidth="4" strokeLinecap="round"/>
+          </NavSvg>
+        </Link>
       </NavItem>
       <NavItem>
-        <Link to="/grief-counseling">GRIEF COUNSELING</Link>
+        <Link to="/grief-counseling">GRIEF COUNSELING
+          <NavSvg>
+            <NavLine x1="2" y1="5" x2="130" y2="5" stroke="white" strokeWidth="4" strokeLinecap="round"/>
+          </NavSvg>
+        </Link>
       </NavItem>
       <NavItem>
-        <Link to="/ngo-crisis-counseling">NGO CRISIS COUNSELING</Link>
+        <Link to="/ngo-crisis-counseling">NGO CRISIS COUNSELING
+          <NavSvg>
+            <NavLine x1="2" y1="5" x2="130" y2="5" stroke="white" strokeWidth="4" strokeLinecap="round"/>
+          </NavSvg>
+        </Link>
       </NavItem>
       <NavItem>
-        <Link to="/contact">CONTACT</Link>
+        <Link to="/contact">CONTACT
+        <NavSvg>
+          <NavLine x1="7" y1="5" x2="95" y2="5" stroke="white" strokeWidth="4" strokeLinecap="round"/>
+        </NavSvg>
+        </Link>
       </NavItem>
-    </NavContainer >
-  </Container >
+    </NavContainer>
+  </NavWrapper>
 )
 
-export default Nav
+export default Nav;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,52 +3,13 @@ import Link from 'gatsby-link'
 import styled from '../../node_modules/styled-components';
 
 import About from './about';
+import Nav from '../components/Nav';
 import { Title, SubTitle } from '../components/Helpers';
-
-// import Header from '../components/Header'
-// import heroImgSrc from '../../public/assets/img/stock/sprout-shadow.jpg';
-// const HeadingFont = {aileron};
-const NavWrapper = styled.div`
-  display: grid;
-  height: 100px;
-  grid-column: 3 / 8;
-  grid-row: 1 / 2;
-  grid-template-columns: 2fr 5fr 1fr;
-`
-
-const NavContainer = styled.ul`
-  display: grid; 
-  grid-gap: 64px;
-  grid-column: 2 / 3;
-  grid-template-columns: repeat(6, 1fr);
-  justify-content: center;
-  align-items: center;
-  color: white;
-  text-shadow: 0 1px 5px rgba(47, 47, 47, 0.25);
-`
-
-const NavItem = styled.li`
-  display: grid;
-  text-align: center;
-  font-size: 18px;
-  letter-spacing: 3px;
-  line-height: 23px;
-  list-style-type: none;
-  border-bottom: 5px solid transparent;
-  padding: 2px;
-  padding-bottom: 5px;
-  border-radius: 5px;
-
-  :hover, :active {
-    border-bottom: 5px solid white;
-  }
-`
 
 const HeroContainer = styled.div`
   display: grid;
   height: 650px;
   grid-template-columns: repeat(8, 1fr);
-  /* grid-template-rows: 1fr 1fr 1fr 1fr 1fr; */
   grid-template-rows: 100px 100px 100px 100px 100px 100px;
   background: linear-gradient(to right, rgba(72, 33, 97, .4) 0%, rgba(189, 68, 92, 0.4) 50%, rgba(204, 133, 0, 0.4) 100%);
 `
@@ -92,31 +53,6 @@ const IndexPage = () => (
     </AboutContainer> */}
     <About/>
   </div>
-)
-
-const Nav = () => (
-  <NavWrapper>
-    <NavContainer>
-      <NavItem>
-        <Link to="/">HOME</Link>
-      </NavItem>
-      <NavItem>
-        <Link to="/about">ABOUT</Link>
-      </NavItem>
-      <NavItem>
-        <Link to="/perinatal-loss">PERINATAL LOSS</Link>
-      </NavItem>
-      <NavItem>
-        <Link to="/grief-counseling">GRIEF COUNSELING</Link>
-      </NavItem>
-      <NavItem>
-        <Link to="/ngo-crisis-counseling">NGO CRISIS COUNSELING</Link>
-      </NavItem>
-      <NavItem>
-        <Link to="/contact">CONTACT</Link>
-      </NavItem>
-    </NavContainer>
-  </NavWrapper>
 )
 
 export default IndexPage
