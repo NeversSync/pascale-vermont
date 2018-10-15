24efd028a6b571e2e2c94a8934511de24c66310f
Nevers <livelyevers@gmail.com>
2018-10-14 18:48:32 -0700

    Fix GA plugin entry in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Pascale Vermont',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-  resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-127500009-1",
-        sampleRate: 5,
-        siteSpeedSampleRate: 10
-      }
+  plugins: ['gatsby-plugin-react-helmet', {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-127500009-1",
+      head: true,
+      sampleRate: 5,
+      siteSpeedSampleRate: 10
+    },
+  },
+  ],
 }
